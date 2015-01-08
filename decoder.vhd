565f7c48edcaabf334464a8c6c54f787fb5f3358
Sandra <macia.sandra9@gmail.com>
2015-01-08 12:21:34 +0100

    isa in proc_package

--- decoder.vhd
+++ decoder.vhd
@@ -15,69 +15,50 @@ end entity decoder;
 
 
 architecture decoder_structure of decoder is
-  
-    -- Formated instruction --
-  signal instr_int : std_logic_vector(15 downto 0);
-
-begin
-
-  -- Data Format Converter -------------------------------------------------------------------------------
-  -- --------------------------------------------------------------------------------------------------------
-data_conv: process(instr_i)
-  variable instr_tmp_v : std_logic_vector(15 downto 0);
-  begin
-    instr_tmp_v := (others => '0');
-      for i in 0 to data_width_c-1 loop
-        instr_tmp_v(i) := instr_i(i);
-      end loop;
-      
-		instr_int <= instr_tmp_v;
-
-end process data_conv;
-  
+begin  
   
  -- Decoder  -------------------------------------------------------------------------------
   -- --------------------------------------------------------------------------------------------------------
-decoder: process(instr_int)
+decoder: process(instr_i)
    begin
       -- defaults --
       ctrl_o                                     <= (others => '0');                  -- all signals disabled
  
-	case (instr_int(15 downto 14)) is
+	case (instr_i(isa_op1_c downto isa_op2_c)) is
 	
 	   when "00" => -- class 0: memory access
       -- -------------------------------------------------------------------	
-			case(instr_int(13 downto 13)) is
+			case(instr_i(isa_mem_load_store_c)) is
 				when "0" => -- Load
-					ctrl_o(ctrl_ra_3_c   downto ctrl_ra_0_c)  	<=  instr_int(12 downto 10)-- operand a register
-					ctrl_o(ctrl_rd_3_c   downto ctrl_rd_0_c)   	<=  instr_int(9 downto 7) 	-- destination register 
-					ctrl_o(ctrl_inm_6_c  downto ctrl_inm_0_c)   	<=  instr_int(7 downto 0) 	-- immediate
+					ctrl_o(ctrl_ra_2_c   downto ctrl_ra_0_c)  	<=  instr_i(isa_mem_load_ra_2_c  downto isa_mem_load_ra_0_c)		-- operand a register
+					ctrl_o(ctrl_rd_2_c   downto ctrl_rd_0_c)   	<=  instr_i(isa_mem_load_rd_2_c  downto isa_mem_load_rd_0_c) 		-- destination register 
+					ctrl_o(ctrl_imm_6_c  downto ctrl_imm_0_c)   	<=  instr_i(isa_mem_load_imm_6_c downto isa_mem_load_imm_0_c) 		-- immediate
 				
 				when "1" => -- Store
-					ctrl_o(ctrl_ra_3_c   downto ctrl_ra_0_c)   	<=  instr_int(12 downto 10)-- operand a register
-					ctrl_o(ctrl_rb_3_c   downto ctrl_rb_0_c)   	<=  instr_int(9 downto 7) 	-- operand b register
-					ctrl_o(ctrl_inm_6_c  downto ctrl_inm_0_c)   	<=  instr_int(9 downto 7)	-- immediate
+					ctrl_o(ctrl_ra_2_c   downto ctrl_ra_0_c)   	<=  instr_i(isa_mem_store_ra_2_c  downto isa_mem_store_ra_0_c)		-- operand a register
+					ctrl_o(ctrl_rb_2_c   downto ctrl_rb_0_c)   	<=  instr_i(isa_mem_store_rb_2_c  downto isa_mem_store_rb_0_c) 	-- operand b register
+					ctrl_o(ctrl_imm_6_c  downto ctrl_imm_0_c)   	<=  instr_i(isa_mem_store_imm_6_c downto isa_mem_store_imm_0_c)	-- immediate
 			end case;
 			
 		
 		when "01" => -- class 1: arithmetic op
       -- -------------------------------------------------------------------	
-			case(instr_int(13 downto 13)) is
+			case(instr_i(isa_alu_c)) is
 				when "0" => -- Op with immediate
-					ctrl_o(ctrl_ra_3_c   downto ctrl_ra_0_c)  	<=  instr_int(10 downto 8) -- operand a register
-					ctrl_o(ctrl_rd_3_c   downto ctrl_rd_0_c)   	<=  instr_int(7 downto 5) 	-- destination register 
-					ctrl_o(ctrl_inm_6_c  downto ctrl_inm_0_c)   	<=  instr_int(4 downto 0) 	-- immediate
+					ctrl_o(ctrl_ra_3_c   downto ctrl_ra_0_c)  	<=  instr_i(isa_alu_imm_ra_2_c downto isa_alu_imm_ra_0_c) 		-- operand a register
+					ctrl_o(ctrl_rd_3_c   downto ctrl_rd_0_c)   	<=  instr_i(isa_alu_imm_rd_2_c downto isa_alu_imm_rd_d_c) 		-- destination register 
+					ctrl_o(ctrl_imm_6_c  downto ctrl_imm_0_c)   	<=  instr_i(isa_alu_imm_imm_4_c downto isa_alu_imm_imm_0_c) 	-- immediate
 				
 				when "1" => -- Op between registers
-					ctrl_o(ctrl_ra_3_c   downto ctrl_ra_0_c)   	<=  instr_int(10 downto 8) -- operand a register
-					ctrl_o(ctrl_rb_3_c   downto ctrl_rb_0_c)   	<=  instr_int(7 downto 5) 	-- operand b register
-					ctrl_o(ctrl_rd_3_c   downto ctrl_rd_0_c)   	<=  instr_int(4 downto 2)	-- immediate
+					ctrl_o(ctrl_ra_3_c   downto ctrl_ra_0_c)   	<=  instr_i(isa_alu_reg_ra_2_c downto isa_alu_reg_ra_0_c) 		-- operand a register
+					ctrl_o(ctrl_rb_3_c   downto ctrl_rb_0_c)   	<=  instr_i(isa_alu_reg_rb_2_c downto isa_alu_reg_rb_0_c) 		-- operand b register
+					ctrl_o(ctrl_rd_3_c   downto ctrl_rd_0_c)   	<=  instr_i(isa_alu_reg_rd_2_c downto isa_alu_reg_rd_0_c)		-- immediate
 			end case;			
 		
 		
 		when "10" => -- class 2: branch
       -- -------------------------------------------------------------------	
-			case(instr_int(13 downto 12)) is
+			case(instr_i(isa_branch_1_c downto isa_branch_0_c)) is
 				when "00" => -- JMP
 
 				when "01" => -- JNE
