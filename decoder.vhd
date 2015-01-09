63e3abbd74ee6f852cdb1c63f5f0fb3c6ef0bafc
Sandra <macia.sandra9@gmail.com>
2015-01-09 12:42:45 +0100

    inserted NOP in ISA

--- decoder.vhd
+++ decoder.vhd
@@ -27,46 +27,51 @@ decoder: process(instr_i)
 
 	case (instr_i(isa_op1_c downto isa_op2_c)) is
 	
-	   when "00" => -- class 0: memory access
+     when op_mem_c => -- class 0: NOP
+      -- -------------------------------------------------------------------	
+      
+      
+      
+	   when op_mem_c => -- class 1: memory access
       -- -------------------------------------------------------------------	
 			case(instr_i(isa_mem_load_store_c)) is
-				when '0' => -- Load
-					ctrl_o(ctrl_rb_imm_c) 								<= '1';
+				when op_mem_load_c => -- Load 
+					ctrl_o(ctrl_rb_imm_c) 								      <= '1';
 					ctrl_o(ctrl_ra_2_c   downto ctrl_ra_0_c)  	<=  instr_i(isa_mem_load_ra_2_c  downto isa_mem_load_ra_0_c);			-- operand a register
-					ctrl_o(ctrl_rd_2_c   downto ctrl_rd_0_c)   	<=  instr_i(isa_mem_load_rd_2_c  downto isa_mem_load_rd_0_c); 			-- destination register 
+					ctrl_o(ctrl_rd_2_c   downto ctrl_rd_0_c)   	<=  instr_i(isa_mem_load_rd_2_c  downto isa_mem_load_rd_0_c); 		-- destination register 
 					ctrl_o   	<=  instr_i(isa_mem_load_imm_6_c downto isa_mem_load_imm_0_c); 		-- immediate
 				
-				when '1' => -- Store
-					ctrl_o(ctrl_ra_2_c   downto ctrl_ra_0_c)   	<=  instr_i(isa_mem_store_ra_2_c  downto isa_mem_store_ra_0_c);						-- operand a register
-					ctrl_o(ctrl_rb_2_c   downto ctrl_rb_0_c)   	<=  instr_i(isa_mem_store_rb_2_c  downto isa_mem_store_rb_0_c); 						-- operand b register
-					imm_o   													<=  "000000000" & instr_i(isa_mem_store_imm_6_c downto isa_mem_store_imm_0_c);	-- immediate
+				when op_mem_store_c => -- Store
+					ctrl_o(ctrl_ra_2_c   downto ctrl_ra_0_c)   	<=  instr_i(isa_mem_store_ra_2_c  downto isa_mem_store_ra_0_c);						      -- operand a register
+					ctrl_o(ctrl_rb_2_c   downto ctrl_rb_0_c)   	<=  instr_i(isa_mem_store_rb_2_c  downto isa_mem_store_rb_0_c); 						    -- operand b register
+					imm_o   													          <=  "000000000" & instr_i(isa_mem_store_imm_6_c downto isa_mem_store_imm_0_c);	-- immediate
 			end case;
 			
 		
-		when "01" => -- class 1: arithmetic op
+		when op_ari_c => -- class 2: arithmetic op
       -- -------------------------------------------------------------------	
 			case(instr_i(isa_alu_c)) is
-				when '0' => -- Op with immediate
-					ctrl_o(ctrl_rb_imm_c) 									<= '1';
+				when op_ari_imm_c => -- Op with immediate
+					ctrl_o(ctrl_rb_imm_c) 									    <= '1';
 					ctrl_o(ctrl_ra_2_c   downto ctrl_ra_0_c)  	<=  instr_i(isa_alu_imm_ra_2_c downto isa_alu_imm_ra_0_c); 							-- operand a register
 					ctrl_o(ctrl_rd_2_c   downto ctrl_rd_0_c)   	<=  instr_i(isa_alu_imm_rd_2_c downto isa_alu_imm_rd_0_c); 							-- destination register 
-					imm_o   													<=  "00000000000" & instr_i(isa_alu_imm_imm_4_c downto isa_alu_imm_imm_0_c); 	-- immediate
+					imm_o   													          <=  "00000000000" & instr_i(isa_alu_imm_imm_4_c downto isa_alu_imm_imm_0_c); 	-- immediate
 				
-				when '1' => -- Op between registers
+				when op_ari_reg_c => -- Op between registers
 					ctrl_o(ctrl_ra_2_c   downto ctrl_ra_0_c)   	<=  instr_i(isa_alu_reg_ra_2_c downto isa_alu_reg_ra_0_c); 		-- operand a register
 					ctrl_o(ctrl_rb_2_c   downto ctrl_rb_0_c)   	<=  instr_i(isa_alu_reg_rb_2_c downto isa_alu_reg_rb_0_c); 		-- operand b register
 					ctrl_o(ctrl_rd_2_c   downto ctrl_rd_0_c)   	<=  instr_i(isa_alu_reg_rd_2_c downto isa_alu_reg_rd_0_c);		-- immediate
 			end case;			
 		
 		
-		when "10" => -- class 2: branch
+		when op_branch_c => -- class 3: branch
       -- -------------------------------------------------------------------	
 			case(instr_i(isa_branch_1_c downto isa_branch_0_c)) is
-				when "00" => -- JMP
+				when op_branch_jmp_c => -- JMP
 
-				when "01" => -- JNE
+				when op_branch_jne_c => -- JNE
 
-				when "10" => -- JE
+				when op_branch_je_c => -- JE
 				
 			end case;
 		
